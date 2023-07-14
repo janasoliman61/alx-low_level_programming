c344aa43c60ca6996c3220185dba99b5472faa78
jana soliman <janasoliman61@gmail.com>
2023-07-13 18:18:59 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - A program that prints a line with puts function
  * Return: 0 (Success)
@@ -7,4 +7,4 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
+i}
