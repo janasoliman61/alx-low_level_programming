34d460ffc027c0aa81e2a68139a02d2b6a926aa3
jana soliman <janasoliman61@gmail.com>
2023-07-13 18:48:33 -0700

    updates

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
+ * main - A program that prints the size of various computer types
  * Return: 0 (Success)
  */
 int main(void)
 {
     char a;
-int b;
-long int c;
-long long int d;
-float f;
+    int b;
+    long int c;
+    long long int d;
+    float f;
+
 printf("Size of a char: %lu byte(s)\n" ,(unsigned long)sizeof(a));
 printf("Size of a int: %lu byte(s)\n" ,(unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n" ,(unsigned long)sizeof(c));
